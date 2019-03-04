e6683cfff4996762fa46cce363f9830e0ca26b93
Atellas23 <alex.batlle01@gmail.com>
2019-03-04 22:05:48 +0100

    update

---
+++ primer/Q2/discreta/particions.cc
@@ -0,0 +1,15 @@
+//aquest programa calcula el nombre total de particions d'un conjunt d'n elements en k parts
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+ll parti(ll n,ll k) {
+	if (k == n or k == 1) return 1;
+	else return parti(n-1,k-1)+k*parti(n-1,k);
+}
+
+int main() {
+	ll n,k;
+	cin >> n >> k;
+	cout << parti(n,k) << endl;
+}
