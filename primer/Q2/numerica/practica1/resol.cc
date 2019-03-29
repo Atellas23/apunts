c41468ceb8fe921f7987ae8cd35eaf0f316fb7c2
Atellas23 <alex.batlle01@gmail.com>
2019-03-29 11:39:45 +0100

    update

--- primer/Q2/numerica/practica1/resol.cc
+++ primer/Q2/numerica/practica1/resol.cc
@@ -1,11 +1,11 @@
 using namespace std;
 
 void resol(double **a, double x[], double b[], int n, int perm[]) {
-    x[n] = b[n]/a[n][n];
-    for (int i = 0; i < n; ++i) {
-        double res = b[i];
-        for (int j = i+1; j < n; ++j) res -= a[i][j]*x[j];
-        res /= a[i][i];
-        x[i] = res;
-    }
+	x[n] = b[n]/a[n][n];
+	for (int i = 0; i < n; ++i) {
+		double res = b[i];
+		for (int j = i+1; j < n; ++j) res -= a[i][j]*x[j];
+		res /= a[i][i];
+		x[i] = res;
+	}
 }
