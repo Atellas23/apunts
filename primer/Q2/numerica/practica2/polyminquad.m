e46ef1222976bd623eca4f06eaeef6072ddc611b
Atellas23 <alex.batlle01@gmail.com>
2019-04-24 23:20:04 +0200

    update

--- primer/Q2/numerica/practica2/polyminquad.m
+++ primer/Q2/numerica/practica2/polyminquad.m
@@ -1,19 +1,22 @@
 function [coefs, norm2res] = polyminquad(x,y,grau,plt)
-%POLYMINQUAD Calcula l'aproximació polinomial de les dades de x i y pel
+%POLYMINQUAD    Calcula l'aproximació polinomial de les dades de x i y pel
 %mètode de mínims quadrats.
 %INPUT:
-%x:vector d'abcisses.
-%y:vector d'ordenades.
-%grau:grau del polinomi que es vol.
-%plt:nombre de punts a utilitzar per dibuixar la gràfica de la funció.
+%x: vector d'abcisses.
+%y: vector d'ordenades.
+%grau: grau del polinomi que es vol.
+%plt: nombre de punts a utilitzar per dibuixar la gràfica de la funció.
+%OUTPUT:
+%coefs: vector de coeficients del polinomi aproximador.
+%norm2res: norma-2 del residu, ||A'A(coefs)=y||_2.
 x=transpose(x);
 y=transpose(y);
 M=vander(x);
 m=size(M,1); %nombre de files
 A=M(:,m-grau:m);
 n=size(A,2); %nombre de columnes
-Q=A'*A; %dimensions m*m
-R=zeros(n);
+Q=A; %dimensions m*(q+1)
+R=zeros(n); %dimensions (q+1)*(q+1)
 
 %Descomposem QR
 for k=1:n-1
@@ -26,10 +29,10 @@ R(n,n)=norm(Q(:,n));
 Q(:,n)=Q(:,n)/R(n,n);
 
 %Solucionem el sistema triangular superior R*a=Q'*y
-x = zeros(m,1);
-x(m)=b(m)/A(m,m);
+a = zeros(n,1);
+a(m)=b(m)/R(m,m);
 for k=m-1:-1:1
-    x(k) = (b(k) - A(k,k+1:n)*x(k+1:n))/A(k,k);
+    a(k) = (a(k) - A(k,k+1:n)*a(k+1:n))/A(k,k);
 end
 res = norm(A*x-b);
 
