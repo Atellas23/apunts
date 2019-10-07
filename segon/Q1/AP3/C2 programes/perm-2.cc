730047c35d0f317189ddb445273b4c13a1d96745
Atellas23 <alex.batlle01@gmail.com>
2019-10-07 10:36:30 +0200

    coses

--- segon/Q1/AP3/C2 programes/perm-2.cc
+++ segon/Q1/AP3/C2 programes/perm-2.cc
@@ -1,35 +1,27 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
+int n;
 
 void write(vector<int>& s) {
     cout << "(" << s[0];
-    for (int i = 1; i < s.size(); ++i)
-      cout << "," << s[i];
+    for (int i = 1; i < s.size(); ++i) cout << "," << s[i];
     cout << ")" << endl;
 }
 
-void gen(int k, vector<int>& s, vector<bool>& mkd) {
-  int n = s.size();
-  if (k == n) /*write(s)*/;
-  else {
-    for (int v = 1; v <= n; ++v) {
-      if (not mkd[v]) {
-	mkd[v] = true;
-	s[k] = v;
-        gen(k+1, s, mkd);
-	mkd[v] = false;
-      }
+void gen(int k, vector<int>& s, vector<bool>& perm) {
+    int n = s.size();
+    if (k == n) write(s);
+    else {
+      
     }
-  }
 }
 
 int main() {
-  int n;
-  cin >> n;
-  vector<int> s(n);
-  vector<bool> mkd(n+1, false);
-  gen(0, s, mkd);
+    cin >> n;
+    vector<int> s(n);
+    vector<bool> perm(n+1, false);
+    gen(0, s, perm);
 }
 
